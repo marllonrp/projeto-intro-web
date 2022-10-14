76f37b9fa6fba5664b592cee07438aa635dfa6da
marllonrp <marllonrp@yahoo.com.br>
2022-10-13 22:23:23 -0300

    Projeto finalizado.

--- index.js
+++ index.js
@@ -31,7 +31,8 @@ const object3 = {
   evolution: true,
   weakenesses: ["Ground"],
   image1: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/135.png",
-  image2: "https://assets.pokemon.com/assets/cms2/img/cards/web/SWSHP/SWSHP_EN_SWSH151.png",
+  image2:
+    "https://assets.pokemon.com/assets/cms2/img/cards/web/SWSHP/SWSHP_EN_SWSH151.png",
   link: "https://www.pokemon.com/us/pokedex/jolteon",
 };
 
@@ -44,7 +45,7 @@ const object4 = {
   weakenesses: ["Water", "Ground", "Rock"],
   image1: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/136.png",
   image2:
-    "https://assets.pokemon.com/assets/cms2/img/cards/web/SWSH7/SWSH7_EN_18.png",
+    "https://assets.pokemon.com/assets/cms2/img/cards/web/SWSHP/SWSHP_EN_SWSH149.png",
   link: "https://www.pokemon.com/us/pokedex/flareon",
 };
 
@@ -55,6 +56,10 @@ const object5 = {
   type: "Psychic",
   evolution: true,
   weakenesses: ["Ghost", "Dark", "Bug"],
+  image1: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/196.png",
+  image2:
+    "https://assets.pokemon.com/assets/cms2/img/cards/web/SWSH7/SWSH7_EN_64.png",
+  link: "https://www.pokemon.com/us/pokedex/espeon",
 };
 
 const object6 = {
@@ -64,6 +69,10 @@ const object6 = {
   type: "Dark",
   evolution: true,
   weakenesses: ["Fairy", "Bug", "Fighthing"],
+  image1: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/197.png",
+  image2:
+    "https://assets.pokemon.com/assets/cms2/img/cards/web/SWSH7/SWSH7_EN_94.png",
+  link: "https://www.pokemon.com/us/pokedex/umbreon",
 };
 
 const object7 = {
@@ -73,6 +82,10 @@ const object7 = {
   type: "Grass",
   evolution: true,
   weakenesses: ["Fire", "Flying", "Ice", "Poison", "Bug"],
+  image1: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/470.png",
+  image2:
+    "https://assets.pokemon.com/assets/cms2/img/cards/web/SWSH7/SWSH7_EN_7.png",
+  link: "https://www.pokemon.com/us/pokedex/leafeon",
 };
 
 const object8 = {
@@ -82,15 +95,23 @@ const object8 = {
   type: "Ice",
   evolution: true,
   weakenesses: ["Fire", "Stell", "Fighthing", "Rock"],
+  image1: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/471.png",
+  image2:
+    "https://assets.pokemon.com/assets/cms2/img/cards/web/SWSHP/SWSHP_EN_SWSH196.png",
+  link: "https://www.pokemon.com/us/pokedex/glaceon",
 };
 
 const object9 = {
   nome: "Sylveon",
   height: 1.0,
   weight: 23.5,
-  type: "Ice",
+  type: "Fairy",
   evolution: true,
   weakenesses: ["Stell", "Poison"],
+  image1: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/700.png",
+  image2:
+    "https://assets.pokemon.com/assets/cms2/img/cards/web/SWSH7/SWSH7_EN_74.png",
+  link: "https://www.pokemon.com/us/pokedex/sylveon",
 };
 
 const allObjects = [];
@@ -106,60 +127,6 @@ allObjects.push(
   object8,
   object9
 );
-
-// 1. Reescrevendo o relatório criado utilizando console.log() que começamos na semana 2, altere a forma que a característica de array dos itens seja escrita como um laço que guarde todos os valores da propriedade array do objeto em uma mesma string. Utilize esta string no relatório.
-
-const modificarArrayString = (array) => {
-  let newArray = [...array];
-  for (objeto in newArray) {
-    newArray[objeto] = { ...newArray[objeto] };
-  }
-
-  for (i = 0; i < newArray.length; i++) {
-    let string = newArray[i].weakenesses.join();
-    newArray[i].weakenesses = string;
-  }
-  return newArray;
-};
-
-// console.log(modificarArrayString(allObjects))
-
-// 2. Ainda no relatório, altere-o para que ele seja criado utilizando um laço. Ou seja, você não deve mais imprimir individualmente cada item do relatório. Cada item deve ser exibido a partir de uma iteração do laço. Para testar, adicione mais um item ao array de objetos, e verifique se ele é exibido corretamente.
-
-// for (i in allObjects){
-//   console.log( allObjects[i])
-// }
-
-// 3. Crie uma função que receba como parâmetro um objeto, e devolva a string do relatório com os dados do objeto.
-
-const dadosObjeto = (objeto) => {
-  let string = "";
-  for (i in objeto) {
-    string += `${i}: ${objeto[i]} `;
-  }
-  return console.log(string);
-};
-
-// dadosObjeto(object1)
-
-// 4. Crie uma função que recebe um array de objetos e uma string. Esta função deve retornar um objeto, e o objeto retornado deve possuir apenas os itens que tenham o nome/título igual à string passada como parâmetro. Caso não exista um item, exiba um ALERT indicando que nenhum item foi encontrado.
-// const stringBusca = prompt ("Busque um nome pokemon: ").toLowerCase()
-
-const filtro = (array, stringg) => {
-  let newArray = [];
-
-  array.filter((titulo) => {
-    if (titulo.nome.toLowerCase() === stringg) {
-      newArray.push(titulo);
-    }
-  });
-  if (newArray.length === 0) {
-    alert("Não foi encontrado nenhum pokemon.");
-  }
-  return newArray;
-};
-
-// console.log(filtro(allObjects, stringBusca));
 
 // Slides
 
@@ -179,10 +146,97 @@ function nextImage() {
   document.getElementById("radio" + count).checked = true;
 }
 
+const creatSectionObject = (objeto) => {
+  // Section
+  const getList = document.getElementById("list-container");
+  const creatSection = document.createElement("section");
+  const newSection = getList.appendChild(creatSection);
+  creatSection.classList.add("items");
+
+  // Divs
+
+  const creatDiv1 = document.createElement("div");
+  const creatDiv2 = document.createElement("div");
+
+  const newDiv1 = newSection.appendChild(creatDiv1);
+  newDiv1.classList.add("cards");
+  const newDiv2 = newSection.appendChild(creatDiv2);
+  newDiv2.classList.add("items-text");
+
+  //Div 1
+
+  const creatFrontCard = document.createElement("div");
+  const creatBackCard = document.createElement("div");
+
+  const newFrontCard = newDiv1.appendChild(creatFrontCard);
+  newFrontCard.classList.add("card__inner");
+  newFrontCard.classList.add("card__inner--front");
+
+  const newBackCard = newDiv1.appendChild(creatBackCard);
+  newBackCard.classList.add("card__inner");
+  newBackCard.classList.add("card__inner--back");
+
+  // Cards
+
+  const creatImgFront = document.createElement("img");
+  const img1 = newFrontCard.appendChild(creatImgFront);
+  img1.setAttribute("src", objeto.image1);
+  const creatImgBack = document.createElement("img");
+  const img2 = newBackCard.appendChild(creatImgBack);
+  img2.setAttribute("src", objeto.image2);
+
+  // Div 2
+
+  const creatH3 = document.createElement("h3");
+  const h3 = newDiv2.appendChild(creatH3);
+  const creatA = document.createElement("a");
+  const aElement = h3.appendChild(creatA);
+  aElement.innerHTML = objeto.nome;
+  aElement.setAttribute("href", objeto.link);
+  aElement.setAttribute("target", "_blank");
+
+  // Lista
+
+  const creatUl = document.createElement("ul");
+  const newUl = newDiv2.appendChild(creatUl);
+  const creatLi1 = document.createElement("li");
+  const creatLi2 = document.createElement("li");
+  const creatLi3 = document.createElement("li");
+  const creatLi4 = document.createElement("li");
+  const creatLi5 = document.createElement("li");
+
+  const li1 = newUl.appendChild(creatLi1);
+  li1.innerHTML = "<strong>height:</strong>" + objeto.height.toString() + " m";
+
+  const li2 = newUl.appendChild(creatLi2);
+  li2.innerHTML = "<strong>weight:</strong>" + objeto.weight.toString() + " Kg";
+
+  const li3 = newUl.appendChild(creatLi3);
+  li3.innerHTML = "<strong>type:</strong>" + objeto.type.toString();
+
+  const li4 = newUl.appendChild(creatLi4);
+  li4.innerHTML =
+    "<strong>weakenesses:</strong>" + objeto.weakenesses.join(", ");
+
+  const li5 = newUl.appendChild(creatLi5);
+  let escrita1LetraM = objeto.evolution.toString();
+  escrita1LetraM =
+    escrita1LetraM[0].toUpperCase() + escrita1LetraM.substring(1);
+  li5.innerHTML = "<strong>evolution:</strong>" + escrita1LetraM;
+};
+
+// Intrudção de objetos na página através do Dom
+
+creatSectionObject(object5);
+creatSectionObject(object6);
+creatSectionObject(object7);
+creatSectionObject(object8);
+creatSectionObject(object9);
+
 // Filp-Cards
 
-document.querySelectorAll(".cards").forEach(
- cards=> {cards.addEventListener("click", (e) => {
+document.querySelectorAll(".cards").forEach((cards) => {
+  cards.addEventListener("click", (e) => {
     if (cards.classList.contains("card--flip")) {
       cards.classList.remove("card--flip");
     } else {
@@ -191,4 +245,42 @@ document.querySelectorAll(".cards").forEach(
   });
 });
 
+// Buscador
 
+const textSearchBar = document.getElementById("name");
+const tagsDeBusca = document.getElementsByTagName("h3");
+const tagsSection = document.querySelectorAll(".items");
+const footer = document.getElementById("footer");
+const arrayInnerText = [];
+
+for (a in tagsDeBusca) {
+  arrayInnerText.push(tagsDeBusca[a].innerText);
+}
+let resultsArray = arrayInnerText.filter((items) => items !== undefined);
+resultsArray = resultsArray.map((items) => items.toLowerCase());
+
+const buscar = (event) => {
+  const texto = textSearchBar.value.toLowerCase();
+
+  tagsSection.forEach((addi) => addi.classList.add("hideItems"));
+
+  for (let i in tagsSection) {
+    if (resultsArray[i] === texto) {
+      tagsSection[i].classList.remove("hideItems");
+    }
+  }
+  let alerta = resultsArray.includes(texto);
+
+    const limpar = () => {
+      if (texto === "") {
+        tagsSection.forEach((addi) => addi.classList.remove("hideItems"));
+        footer.classList.remove("fixed-footer")
+      }else if (texto !== "" && alerta === false){
+        alert("Nenhum resultado encontrado");
+        footer.classList.add("fixed-footer");
+      }
+    };
+
+    limpar();
+  
+};
